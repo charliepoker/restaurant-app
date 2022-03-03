7b9192c048dde61f738601c9a905ec7220370f6d
charliepoker <iheanachocharlie@gmail.com>
2022-03-03 22:39:04 +0100

    moidified controllers

--- models/review.js
+++ models/review.js
@@ -2,18 +2,6 @@ const mongoose = require("mongoose");
 
 const ReviewSchema = mongoose.Schema(
   {
-    rating: {
-      type: Number,
-      min: 1,
-      max: 5,
-      required: [true, "Please provide rating"],
-    },
-    title: {
-      type: String,
-      trim: true,
-      required: [true, "Please provide review title"],
-      maxlength: 100,
-    },
     comment: {
       type: String,
       required: [true, "Please provide review text"],
