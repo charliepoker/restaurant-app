5147fa0a76302e7f3c0fdbf869371c46f3cfa245
charliepoker <iheanachocharlie@gmail.com>
2022-03-07 10:43:40 +0100

    modified

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,7 +2,8 @@ const jwt = require("jsonwebtoken");
 
 const createToken = (id) => {
   const token = jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "1h",
+    expiresIn: 60 * 60,
+    algorithm: "HS256",
   });
   return token;
 };
